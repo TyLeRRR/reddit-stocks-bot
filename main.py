2aa25b136f36c722afb5c313ba5481014ad72b0e
Taras <sologub.taras@gmail.com>
2017-06-11 19:53:00 +0200

    Save new post_id to the post_ids.txt
    Scan incoming json response for the presented posts ids. The post id will be saved to the post_ids.txt only if it is not present there.

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ import requests.auth
 import time
 import auth
 
-access_token = auth.get_access_token()
 
-
-def get_hot_topic():
-    headers = {"Authorization": "bearer " + access_token, "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+def get_hot_topic(token):
+    headers = {"Authorization": "bearer " + token, "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
     response = requests.get("https://oauth.reddit.com/r/stocks/hot", headers=headers)
-    print response.json()
+    temp_response = response.json()
+
+    for object in temp_response['data']['children']:
+        id = object['data']['id']
+        save_post_id(id)
+
+
+def save_post_id(post_id):
+    file = open('post_ids.txt', 'r+')
+
+    if post_id in file.read():
+        print "Current id already exists"
+    else:
+        print "Writing id :" + post_id + " into file"
+        file.write("%s\n" % post_id)
+    file.close()
+
+get_hot_topic(auth.get_access_token())
+
