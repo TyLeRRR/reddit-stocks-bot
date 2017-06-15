749c895173c37989cc2db6cc122ba0c6d40cb236
Taras <sologub.taras@gmail.com>
2017-06-15 17:06:39 +0200

    Create tweet.py

--- tweet.py
+++ tweet.py
@@ -1,10 +1,10 @@
 import tweepy
 import requests
 
-consumer_key = "qEkMcTsYo4Mt4eoaXs0oMagq8"
-consumer_secret_key = "ErUWC4AGZcZn80Mbj4V3haGjA2x1cjMDfMNooOdBYFjceKEsx3"
-access_token = "873550294310940672-bwL3XoaNfA4Q5vQtTrB7yCDAnTg3Ecp"
-access_token_secret = "HwXvpl90z4UcwlkpgvuRxuNWM4gj8HujShjjst5uIOpYR"
+consumer_key = **********
+consumer_secret_key = *********
+access_token = *********
+access_token_secret = *********
 
 
 def authenticate():
